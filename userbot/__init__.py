fba8657bf180ec613741151d29aec886386b7d3f
@bootIoop <50399358+arraqueen@users.noreply.github.com>
2020-03-28 16:11:58 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -115,6 +115,9 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
+# Telegraph
+TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
+
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
